3e5c79e227f9c5500c7502f801773cc07927081f
unknown <mstevens9335@yahoo.com>
2019-02-19 21:30:38 -0500

    almost finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,8 @@
                  var guessedLetters = [];
                  var word=[];
                  var lengthWord;
+                 var match;
+                 var chars;
 
                  //display
                  document.getElementById("wins").innerHTML = wins;
@@ -22,8 +24,8 @@
                   
                   //console log to test
                   console.log(word);
-                  var chars = word.split('');
-                  console.log(chars);
+                  chars = word.split('');
+                   console.log(chars);
                   return chars;
                 }
                 
@@ -33,36 +35,30 @@
                 // determine number of letters in word
                 function wordLength(){
                   lengthWord = word.length;
-                  console.log(lengthWord);
-
-                  // display underscores for the number of letters in the word
-                  for(var i = 0; i<lengthWord; i++){
-                    blank[i]="_";
-                  }
-                  document.getElementById("word").innerHTML = blank;
-                
+                  // console.log(lengthWord);
                 }
 
                 // call function to get word length
                 wordLength();
 
-                // determine if word and blank arrays are equal
-                function arraysEqual(chars, blank) {
-                
-                  for(var i = chars.length; i--;) {
-                      if(word[i] !== blank[i])
-                          return false;
-                  }
-                  return true;
-                 }
 
-               
+              // display underscores for the number of letters in the word
+              function setBlanks(){
+              for(var i = 0; i<lengthWord; i++){
+                blank[i]="_";
+              }
+              // console.log(blank);
+              document.getElementById("word").innerHTML = blank;
+            }
+               setBlanks();
 
                 // function to get user guess
                 document.onkeyup = function(event) {
 
                 // Determines which key was pressed.
                 var userGuess = event.key;
+               
+
                 
                 // determines if the letter the user guessed is in the word or not
                 if(word.indexOf(userGuess) !=-1){
@@ -75,6 +71,7 @@
                     }
                    }
                    document.getElementById("word").innerHTML = blank;
+                  //  console.log(blank);
                 }
 
                 // if letter isnt in word
@@ -94,41 +91,27 @@
 
                 }
 
+             var pc = chars.join();
+             var client = blank.join();
+             
+             if(pc===client){
+              wins++;
+              // display new wins
+              document.getElementById("wins").innerHTML = wins;
 
-                // arraysEqual();
-                // if(arraysEqual==true){
-                //   wins++;
-                //       document.getElementById("wins").innerHTML = wins;
-                //       compGuess();
-                //     wordLength();
-                //     guesses = 10;
-                //     // display new guesses
-                //     document.getElementById("guesses").innerHTML = guesses;
-                //      // display guessed letters
-                //      guessedLetters = [];
-                //   document.getElementById("guessed").innerHTML = guessedLetters;
-                // }
-
-
-
-                // determines if the user wins or loses
-                // for(var i = word.length; i--;) {
-                //   if(word[i] == blank[i]){
-
-                //     wins++;
-                //     document.getElementById("wins").innerHTML = wins;
-                //     compGuess();
-                //   wordLength();
-                //   guesses = 10;
-                //   // display new guesses
-                //   document.getElementById("guesses").innerHTML = guesses;
-                //    // display guessed letters
-                //    guessedLetters = [];
-                // document.getElementById("guessed").innerHTML = guessedLetters;
-                // }
-               
-                // }
-
+            
+              // display guessed letters
+              guessedLetters = [];
+            document.getElementById("guessed").innerHTML = guessedLetters;
+            // start game over
+            compGuess();
+            wordLength();
+            setBlanks();
+            guesses = 10;
+            // display new guesses
+            document.getElementById("guesses").innerHTML = guesses;
+            
+             }
                
 
                 if(guesses===0){
